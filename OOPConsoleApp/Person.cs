b59d3a00246d7ead64cae9cab6889d123671d2d2
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2021-03-01 11:06:08 +0100

    Add project files.

---
+++ OOPConsoleApp/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPConsoleApp
+{
+    class Person
+    {
+        //fiealds
+        string firstName;
+        string lastName;
+        Gender gender;
+        public int age;//defualt assigned 0
+
+        public Person()
+        {
+            firstName = "John";
+            lastName = "Doe";
+            gender = Gender.Other;
+        }
+        public Person(string firstName, string lastName, int age, Gender gender)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public string Info()
+        {
+            return $"Name: {firstName} {lastName}\nGender: {gender}\nAge: {age}";
+        }
+
+    }
+
+    enum Gender
+    {
+        Man,
+        Woman,
+        Other
+    }
+}
---
+++ OOPConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOPConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Person userInfo = new Person();
+
+            Console.WriteLine(userInfo.Info());
+
+            Person me = new Person("Ulf", "Bengtsson", 38, Gender.Man);
+
+            Console.WriteLine(me.Info());
+
+        }
+    }
+}
